9aa1f6749ad948c4df1aacd3545298869506f379
dmattox <daniel.e.mattox.gr@dartmouth.edu>
2020-11-03 08:58:19 -0500

    small tweak to run on cluster

--- scripts/prediction/cluster/startJobs.sh
+++ scripts/prediction/cluster/startJobs.sh
@@ -3,7 +3,7 @@
 cnt=1
 while [ $cnt -le 15 ]; do
 mkdir ./$cnt
-cp /Users/dmattox/cbk/lec_gly_binding/scripts/prediction/cluster/runTrainValidate.pbs ./$cnt
+cp /dartfs-hpc/rc/home/y/f002tsy/cbklab/Mattox/lec_gly_binding/scripts/prediction/cluster/runTrainValidate.pbs ./$cnt
 cd ./$cnt
 echo "Submitting ${cnt}"
 mksub runTrainValidate.pbs
