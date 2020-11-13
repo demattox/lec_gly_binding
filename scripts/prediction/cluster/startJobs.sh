792dc95afc89001dd3c9d53416b93eb719acbbd6
Daniel Mattox <xfdmattox@gmail.com>
2020-11-12 17:32:54 -0700

    cluster scripts

--- scripts/prediction/cluster/startJobs.sh
+++ scripts/prediction/cluster/startJobs.sh
@@ -3,10 +3,10 @@
 cnt=1
 while [ $cnt -le 15 ]; do
 mkdir ./$cnt
-cp /dartfs-hpc/rc/home/y/f002tsy/cbklab/Mattox/lec_gly_binding/scripts/prediction/cluster/runTrainValidate.pbs ./$cnt
+cp /dartfs-hpc/rc/home/y/f002tsy/cbklab/Mattox/lec_gly_binding/scripts/prediction/cluster/${1} ./$cnt
 cd ./$cnt
 echo "Submitting ${cnt}"
-mksub runTrainValidate.pbs
+mksub ${1}
 cd ..
 let cnt=cnt+1
 done
